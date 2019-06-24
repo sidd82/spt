07f87665dfc9384a328d66d0695155a939955c3d
Kartish Pitale <pitale.kartish@gmail.com>
2019-06-24 23:35:36 +0530

    Initial Front Page UI

--- src/B2C/Home/Home.jsx
+++ src/B2C/Home/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
+// Style Import
+import "./homestyle.css";
+
+// Component Importt
+import TopBar from "../Symbols/TopBar/TopBar";
+import NavBar from "../Symbols/NavBar/NavBar";
+import MainArea from "../Home/MainArea/MainArea";
+import Footer from "../Symbols/Footer/Footer";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="main-container-spt">
+        <TopBar />
+
+        <NavBar />
+
+        <MainArea />
+
+        <div className="infoarea-wrapper-spt">
+          <h1>Info Area</h1>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 };
