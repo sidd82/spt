9124ca14f0445ed6a2459fcec774e37e5e0de9af
Kartish Pitale <pitale.kartish@gmail.com>
2019-06-24 23:15:36 +0530

    Initial Context State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+// Initial Store & Reducer
+import initialState from "./context/store";
+import searchReducer from "./context/reducer/searchReducer";
+
+// Context Provider
+import { StateProvider } from "./context/context";
+
+// Component Import
+import Home from "./B2C/Home/Home";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <StateProvider initialState={initialState} reducer={searchReducer}>
+      <Home />
+    </StateProvider>
   );
 }
 
